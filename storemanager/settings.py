9e4b0648add79f573c248f6f709066f353b93149
code-rishav <raj08rishav@gmail.com>
2023-06-27 12:45:57 +0530

    updated backend details

--- storemanager/settings.py
+++ storemanager/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = "storemanager.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": 'inventorydb',
+        "USER": 'admin',
+        "PASSWORD": 'Iamrishav@0804',
+        "HOST": 'localhost'
     }
 }
 
